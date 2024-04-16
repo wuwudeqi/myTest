daea3afa369b675694ea762a6a736334dd837287
wuwudeqi <920776574@qq.com>
2024-04-16 14:53:05 +0800

    dev-2

--- src/future/FutureTaskDemo.java
+++ src/future/FutureTaskDemo.java
@@ -7,6 +7,7 @@ public class FutureTaskDemo {
 
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("321");
         Callable<String> callable =
                 new Callable<String>() {
                     @Override
